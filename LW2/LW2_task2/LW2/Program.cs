b77da354b9849bbd7769931b34b9ad9e5094a453
Akira Shirase <justsaynya@gmail.com>
2020-03-22 21:55:58 +0300

    LW2

---
+++ LW2/LW2_task2/LW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+            else
+            {
+                Console.WriteLine($"Number of arguments: {args.Length}");
+                Console.WriteLine($"Arguments: {string.Join(" ", args)}");
+            }
+            
+        }
+    }
+}
---
+++ LW2/LW2_task3/LW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe <input string>");
+            }
+            else
+            {
+                Console.WriteLine(args[0].Distinct().ToArray());
+            }
+
+        }
+    }
+}
